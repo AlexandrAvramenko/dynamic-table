5ca896ad98b11c14844d475f2afdd38bad9c1b77
Alexandr Avramenko <alexandr_avramenko@MacBook-Pro-EU.local>
2020-01-04 19:00:45 +0200

    add pagination

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import ReactPaginate from "react-paginate";
 import Loader from "./Loader/Loader";
 import Table from "./Table/Table";
 import DetailRowView from "./DetailRowView/DetailRowView";
@@ -6,14 +7,14 @@ import ModeSelector from "./ModeSelector/ModeSelector";
 import _ from "lodash";
 
 class App extends Component {
-
   state = {
     isModeSelected: false,
     isLoding: false,
     data: [],
     sort: "asc", //desc
     sortField: "id",
-    row: null
+    row: null,
+    currentPage: 0
   };
 
   async fetchData(url) {
@@ -23,58 +24,91 @@ class App extends Component {
     this.setState({
       isLoading: false,
       data: _.orderBy(data, this.state.sortField, this.state.sort)
-    })
+    });
   }
 
   onSort = sortField => {
     const clonedData = this.state.data.concat();
     const sort = this.state.sort === "asc" ? "desc" : "asc";
 
-    const data = _.orderBy(clonedData, sortField, sort)
+    const data = _.orderBy(clonedData, sortField, sort);
 
     this.setState({
       data,
       sort,
       sortField
     });
-  }
+  };
 
   modeSelectHandler = url => {
     this.setState({
       isModeSelected: true,
       isLoading: true
-    })
+    });
 
-    this.fetchData(url)
-  }
+    this.fetchData(url);
+  };
 
-  onRowSelect = row => (
-    this.setState({row})
-  )
+  onRowSelect = row => {
+    this.setState({ row });
+  };
+
+  pageChangeHandler = ({selected}) => {
+    this.setState({currentPage: selected})
+  };
 
   render() {
+    const pageSize = 50;
+
     if (!this.state.isModeSelected) {
       return (
         <div className="container">
-          <ModeSelector onSelect={this.modeSelectHandler}/>
+          <ModeSelector onSelect={this.modeSelectHandler} />
         </div>
-      )
+      );
     }
+
+    const displayData = _.chunk(this.state.data, pageSize)[this.state.currentPage]
+
     return (
-    <div className="container">
-      {this.state.isLoading ? (
+      <div className="container">
+        {this.state.isLoading ? (
           <Loader />
         ) : (
           <Table
-            data={this.state.data}
+            data={displayData}
             onSort={this.onSort}
             sort={this.state.sort}
             sortField={this.state.sortField}
             onRowSelect={this.onRowSelect}
           />
         )}
+        {this.state.data.length > pageSize ? (
+          <div className="d-flex justify-content-center">
+            <ReactPaginate
+              previousLabel={"<"}
+              nextLabel={">"}
+              breakLabel={"..."}
+              breakClassName={"break-me"}
+              pageCount={20}
+              marginPagesDisplayed={2}
+              pageRangeDisplayed={5}
+              onPageChange={this.pageChangeHandler}
+              containerClassName={"pagination"}
+              activeClassName={"active"}
+              pageClassName={"page-item"}
+              pageLinkClassName={"page-link"}
+              previousClassName={"page-item"}
+              nextClassName={"page-item"}
+              previousLinkClassName={"page-link"}
+              nextLinkClassName={"page-link"}
+              forcePage={this.state.currentPage}
+            />
+          </div>
+        ) : null}
         {this.state.row ? <DetailRowView person={this.state.row} /> : null}
-    </div>);
+      </div>
+    );
   }
 }
 
